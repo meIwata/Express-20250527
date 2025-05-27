ffec89f24900bae23abd8025a12da316427f7db0
meiwata <waleilulu@gmail.com>
2025-05-27 14:50:21 +0800

    基本路由get

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+
+// 基本路由
+// 用 get 在 /hello 路由
+// 返回 "Hello World!" 字符串
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
